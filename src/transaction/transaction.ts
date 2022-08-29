7c5790a48a5c7d0daaba8222adc27abd5e3cedf4
doomsower <K.Kuznetcov@gmail.com>
2022-08-29 16:46:39 +0300

    fix: bring back old deploy function signature and add deployWithOptions (#11)
    Co-authored-by: doomsower <K.Kyznetcov@gmail.com>

--- src/transaction/transaction.ts
+++ src/transaction/transaction.ts
@@ -1,17 +1,34 @@
 import { TransactionReceipt } from "@ethersproject/providers";
-import { formatBN } from "@gearbox-protocol/sdk";
+import {
+  formatBN,
+  GOERLI_NETWORK,
+  HARDHAT_NETWORK,
+  LOCAL_NETWORK,
+} from "@gearbox-protocol/sdk";
 import { Contract, ContractFactory, ContractTransaction } from "ethers";
 import { ethers } from "hardhat";
 import { Logger } from "tslog";
 
 import { Verifier } from "../verifier";
 
+const waitingTime = async () => {
+  const accounts = await ethers.getSigners();
+  const deployer = accounts[0];
+  const chainId = await deployer.getChainId();
+
+  return chainId === LOCAL_NETWORK || chainId === HARDHAT_NETWORK
+    ? 0
+    : chainId === GOERLI_NETWORK
+    ? 2
+    : 4;
+};
+
 export async function waitForTransaction(
   transaction: Promise<ContractTransaction>,
   logger?: Logger,
 ): Promise<TransactionReceipt> {
   const request = await transaction;
-  const txReceipt = await request.wait();
+  const txReceipt = await request.wait(await waitingTime());
 
   if (logger) {
     logger.debug(`Tx: ${txReceipt.transactionHash}`);
@@ -34,26 +51,20 @@ export type ContractFactoryConstructor<T extends ContractFactory> = new (
 ) => T;
 export type ContractConstructor<T extends Contract> = new (...args: any[]) => T;
 
-export interface DeployOptions {
-  logger?: Logger;
-  verifier?: Verifier;
-  confirmations?: number;
-}
-
 export async function deploy<T extends Contract>(
   name: string,
-  options: DeployOptions | undefined,
+  logger: Logger | undefined,
   ...args: any[]
 ): Promise<T> {
   const artifact = await ethers.getContractFactory(name);
 
   const contract = (await artifact.deploy(...args)) as T;
-  options?.logger?.debug(`Deploying ${name}...`);
+  logger?.debug(`Deploying ${name}...`);
   await contract.deployed();
-  const txReceipt = await contract.deployTransaction.wait();
-  options?.logger?.debug(`Deployed ${name} to ${contract.address}`);
-  options?.logger?.debug(`Tx: ${txReceipt.transactionHash}`);
-  options?.logger?.debug(
+  const txReceipt = await contract.deployTransaction.wait(await waitingTime());
+  logger?.debug(`Deployed ${name} to ${contract.address}`);
+  logger?.debug(`Tx: ${txReceipt.transactionHash}`);
+  logger?.debug(
     `Gas used: ${txReceipt.gasUsed.toString()} @ ${formatBN(
       txReceipt.effectiveGasPrice,
       9,
@@ -62,16 +73,26 @@ export async function deploy<T extends Contract>(
       18,
     )} ETH`,
   );
+  return contract;
+}
+
+export interface DeployOptions {
+  logger?: Logger;
+  verifier?: Verifier;
+}
+
+export async function deployWithOptions<T extends Contract>(
+  name: string,
+  options: DeployOptions | undefined,
+  ...args: any[]
+): Promise<T> {
+  const contract = await deploy<T>(name, options?.logger, ...args);
 
   if (options?.verifier) {
     options.verifier.addContract({
       address: contract.address,
       constructorArguments: args,
     });
-  }
-
-  if (options?.confirmations) {
-    await contract.deployTransaction.wait(options.confirmations);
   }
 
   return contract;
