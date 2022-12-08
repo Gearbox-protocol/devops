192d7302dfc24262b8a24311be0fb1ab0df2de23
Mikael <mikael.lazarev@gmail.com>
2022-12-08 22:55:16 +0100

    fix: remove waffle

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import "@nomiclabs/hardhat-ethers";
 
 export * from "./logger/loggedDeployer";
 export * from "./transaction/transaction";
-export * from "./utils/balanceComparator";
-export * from "./utils/expect";
 export * from "./utils/getNetwork";
 export * from "./utils/impersonate";
 export * from "./utils/RetryProvider";
--- src/utils/expect.ts
+++
@@ -1,5 +0,0 @@
-import chai from "chai";
-import { solidity } from "ethereum-waffle";
-
-chai.use(solidity);
-export const { expect } = chai;
